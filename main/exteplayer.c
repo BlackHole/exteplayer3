9bedc5d05ef2502419269e3b8cb3ffe84ba4579d
Ev0-BH <Ev0_BhT@outlook.com>
2026-09-24 01:05:47 +0100

    test

--- main/exteplayer.c
+++ main/exteplayer.c
@@ -1184,7 +1184,7 @@ int main(int argc, char* argv[])
             }
             case 'r':
             {
-                commandRetVal = g_player->output->Command(g_player, OUTPUT_CLEAR, "a");
+                commandRetVal = g_player->output->Command(g_player, OUTPUT_CLEAR, "s");
                 break;
             }
             case 'p':
